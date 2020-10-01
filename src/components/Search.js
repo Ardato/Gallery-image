5db7d31e863cc4e3a7efbd1dbb5f7a648af2116f
ardato belay <aradatobelay@gmaill.com>
2020-10-01 18:06:49 +0300

    initial commit

---
+++ src/components/Search.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const Search = ({ getQuery }) => {
+  const [text, setText] = useState("");
+  const onChange = (query) => {
+    setText(query);
+    getQuery(query);
+  };
+
+  return (
+    <section className="search">
+      <form>
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => onChange(e.target.value)}
+          value={text}
+        />
+      </form>
+      <div></div>
+    </section>
+  );
+};
+
+export default Search;
