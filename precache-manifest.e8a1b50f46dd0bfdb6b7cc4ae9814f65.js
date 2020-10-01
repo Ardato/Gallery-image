6bdea56fd53a892f0500fab489a0fe76e523e707
ardato belay <aradatobelay@gmaill.com>
2020-10-01 18:19:11 +0300

    Updates

---
+++ precache-manifest.e8a1b50f46dd0bfdb6b7cc4ae9814f65.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c68f9c629d8e0478a4a1ac9c1cf942",
+    "url": "/Gallery-image/index.html"
+  },
+  {
+    "revision": "3c35848fa5d1611234cd",
+    "url": "/Gallery-image/static/css/main.de588e20.chunk.css"
+  },
+  {
+    "revision": "94bfddc2c731c70137dd",
+    "url": "/Gallery-image/static/js/2.6dac0812.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gallery-image/static/js/2.6dac0812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c35848fa5d1611234cd",
+    "url": "/Gallery-image/static/js/main.2b0ff564.chunk.js"
+  },
+  {
+    "revision": "139a93afa3d33706f3a3",
+    "url": "/Gallery-image/static/js/runtime-main.914e4b93.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Gallery-image/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "6c980c6d2dd2c87211686165802f3260",
+    "url": "/Gallery-image/static/media/our-gallery.6c980c6d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Gallery-image/static/media/spinner.235b9b3e.gif"
+  }
+]);
